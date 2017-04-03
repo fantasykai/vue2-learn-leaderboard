02823bb51f31a93ff22d2e5c2f56933091e811f5
fantasykai <makren@126.com>
2017-04-03 12:28:26 +0800

    kai.fantasy: 练习v-show, v-bind, methods等

--- js/main.js
+++ js/main.js
@@ -1,12 +1,28 @@
 var vm = new Vue({
     el: '#user-list',
     data: {
-        ranking: []
+        ranking: [],
+        odd: 'odd',
+        even: 'even'
+
     },
     mounted: function () {
         axios.get('static/mockdata.json').then((res) => {
             this.ranking = res.data._items
             console.log("ranking:" + res.data._items)
         });
+    },
+    methods: {
+        medals: function (index) {
+            if (index === 0) {
+                return 'img/gold.png';
+
+            } else if (index === 1) {
+                return 'img/silver.png';
+
+            } else if (index === 2) {
+                return 'img/cooper.png';
+            }
+        }
     }
 });
