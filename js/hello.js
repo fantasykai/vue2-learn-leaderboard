f18bbfc5a9f0162f1a8f23713cd645416d291b2f
fantasykai <makren@126.com>
2017-08-04 19:35:26 +0800

    kai.fantasy: hello.js 基本演示

---
+++ js/hello.js
@@ -0,0 +1,14 @@
+/**
+ * Created by kai.fantasy on 2017/8/4 0004.
+ */
+new Vue({
+    el: '#hello',
+    data: {
+        name: '琳云小伙伴'
+    },
+    methods: {
+        initName() {
+            this.name = '琳云小伙伴'
+        }
+    }
+});
